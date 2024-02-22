4b78ead365b4662b657d53d7e08d7e3bdb9233bf
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2024-02-22 00:41:16 -0600

    portfolio content added

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./portfolio.scss";
+import msImage from "../images/millionSpaces.png";
+import goldenGateImage from "../images/goldenGate.PNG";
 
 function Portfolio() {
     return (
@@ -11,10 +13,22 @@ function Portfolio() {
                         <li className="portfolio">
                             <div className="inner">
                                 <a href="https://millionspaces.com/Singapore" target="blank">
-                                    <div className="image"></div>
+                                    <div className="image" style={{backgroundImage:`url(${msImage})`}}></div>
                                     <div className="details">
                                         MillionSpaces is an online platform where customers can book their working space
                                         according to the facilities provided and also according to the environment they prefer
+                                    </div>
+                                    <div className="read-more-text">READ MORE</div>
+                                </a>
+                            </div>
+                        </li>
+                        <li className="portfolio">
+                            <div className="inner">
+                                <a href="https://thegoldengate.uk/" target="blank">
+                                    <div className="image" style={{backgroundImage:`url(${goldenGateImage})`}}></div>
+                                    <div className="details">
+                                        Development of a student-centric application, "Golden Gate," designed to enhance the
+                                        educational experience through an interactive user interface.
                                     </div>
                                     <div className="read-more-text">READ MORE</div>
                                 </a>

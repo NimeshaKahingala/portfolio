e459dcdc8c1d77ebb9efaa40a67b1e0b4fcb471e
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2024-02-24 20:00:55 -0600

    portfolio styles added

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,14 +7,16 @@ function Portfolio() {
     return (
         <div className="portfolio-page">
             <div className="container">
-            <div className="section-name">portfolio</div>
+                <div className="section-name">portfolio</div>
                 <h3 className="title">Selected Portfolios</h3>
                 <div className="list-wrapper">
                     <ul className="portfolio-list">
                         <li className="portfolio">
                             <div className="inner">
                                 <a href="https://millionspaces.com/Singapore" target="blank">
-                                    <div className="image" style={{backgroundImage:`url(${msImage})`}}></div>
+                                    <div className="image-wrap">
+                                        <div className="image" style={{ backgroundImage: `url(${msImage})` }}></div>
+                                    </div>
                                     <div className="details">
                                         MillionSpaces is an online platform where customers can book their working space
                                         according to the facilities provided and also according to the environment they prefer
@@ -26,7 +28,9 @@ function Portfolio() {
                         <li className="portfolio">
                             <div className="inner">
                                 <a href="https://thegoldengate.uk/" target="blank">
-                                    <div className="image" style={{backgroundImage:`url(${goldenGateImage})`}}></div>
+                                    <div className="image-wrap">
+                                        <div className="image" style={{ backgroundImage: `url(${goldenGateImage})` }}></div>
+                                    </div>
                                     <div className="details">
                                         Development of a student-centric application, "Golden Gate," designed to enhance the
                                         educational experience through an interactive user interface.

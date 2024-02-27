a3594d2560fbef31c79d149d32750ce4c896660d
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2024-02-26 22:38:57 -0600

    projects data added

---
+++ src/components/data.js
@@ -0,0 +1,43 @@
+import img_millionSpaces from "../images/millionSpaces.png";
+import img_goldenGate from "../images/goldenGate.PNG";
+
+export const PROJECTS = [{
+    title: "MillionSpaces",
+    image: {img_millionSpaces},
+    description: "MillionSpaces is an online platform where customers can book their working space according to the facilities provided and also according to the environment they prefer",
+    url: "https://millionspaces.com/Singapore",
+    responsibilities : "Oversaw the transformation of design concepts into interactive web interfaces, enhancing the platform's user engagement and functionality. Developed responsive React components, ensuring a consistent user experience across various devices and platforms. Contributed to feature enhancements, refined user input validation processes, and error handling mechanisms. Successfully integrated complex backend APIs, including Google Map API, to augment the platform's capabilities.",
+    technology: "HTML, CSS, JavaScript, React, Ant Design, Google Map API, JWT"
+},
+{
+    title: "Golden Gate",
+    image: {img_goldenGate},
+    description: "Development of a student-centric application, `'Golden Gate,'` designed to enhance the educational experience through an interactive user interface.",
+    url: "https://thegoldengate.uk/",
+    responsibilities : "Developed a user-friendly interface, prioritizing accessibility and engagement for the student user base.",
+    technology: "HTML, CSS, JavaScript, React"
+},
+{
+    title: "SqaudGurus",
+    image: {img_goldenGate},
+    description: "Squad Gurus is an online platform where clients can choose a skilled developer team to bring their business ideas to life within the agreed timeframe.",
+    url: "https://www.squadgurus.com/",
+    responsibilities : "Implementing cross-browser compatible, reusable, and maintainable code while adhering to web standards and best practices. Collaborate closely with other developers and designers to create pixel-perfect visual components to deliver intuitive and engaging user experiences. Played a crucial role in troubleshooting UI problems, and delivering solutions that enhanced overall user experience and interface stability. Scheduled and monitored knowledge transfer (KT) sessions based on project needs. Engaged in project estimations, contributing to accurate planning and resource allocation. Mentored and groomed newly joined team members, enhancing team skills and coherence.",
+    technology: "HTML, CSS, JavaScript, React, React-md, JWT"
+},
+{
+    title: "1Billion Technology Website",
+    image: {img_goldenGate},
+    description: "Overhaul and implement the official website of 1Billion Technology, adapting it to new and modern design principles.",
+    url: "",
+    responsibilities : "Led the front-end development process, transforming complex wireframes and sophisticated designs into dynamic web application interfaces, with a priority on mobile-first design principles to ensure optimal user experience across various devices. Ensured the creation of high-quality, cross-browser compatible, and maintainable code, emphasizing adherence to contemporary web standards and best practices.",
+    technology: " HTML, CSS, JavaScript, React, Tailwind CSS"
+},
+{
+    title: "Banyan Restaurant",
+    image: {img_goldenGate},
+    description: "Development of a compelling online presence for Banyan Restaurant, focusing on responsive design and user engagement",
+    url: "https://www.banyanrestaurant.co.uk/",
+    responsibilities : "Translated wireframes and designs into web application interfaces. Implemented cross-browser compatible, reusable, and maintainable code while adhering to web standards and best practices.",
+    technology: "HTML, CSS, JavaScript, React"
+}];

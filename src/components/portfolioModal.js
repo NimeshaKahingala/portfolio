6b4cce57e6f4c97c227ae636715c1a55beb4e4d9
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2024-02-26 23:11:27 -0600

    portfolio modal updated

--- src/components/portfolioModal.js
+++ src/components/portfolioModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PROJECTS } from "../components/data";
+import "./portfolioModal.scss";
 
 function PortfolioModal(props) {
 
@@ -7,8 +8,14 @@ function PortfolioModal(props) {
         <>
 
             <div className='modal-wrap'>
-                <p>{props.data?.title}</p>
-                <p>{props.data?.description}</p>
+                <div className="img-wrap"><img className="image"></img>image</div>
+                <h3 className="title">{props.data?.title}</h3>
+                <p className="description">{props.data?.description}</p>
+                <div className="technology">Technologies used : <span>{props.data?.technology}</span></div>
+                <div className="url">Website URL : <a href="https://millionspaces.com/Singapore" target="blank" className="link">millionspaces.com/Singapore</a></div>
+                <div className="website-images">
+                    <img></img>img1
+                </div>
             </div>
 
 

c2bd694417fea2cbce92dd2da7b1396caa5824b5
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2024-02-27 19:16:40 -0600

    contact details updated

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,15 +9,32 @@ function Contact() {
     return (
         <div className="contact-page">
             <div className="container">
-            <div className="section-name">Contact</div>
+                <div className="section-name">Contact</div>
                 <h3 className="title">Get in Touch</h3>
                 <div className="text">It all starts with a discussion, please feel free to contact me through</div>
                 <div className="details">
                     <ul>
-                        <li><a href=""><img className="icon" src={fbIcon}></img></a></li>
-                        <li><a href=""><img className="icon" src={linkedinIcon}></img></a></li>
-                        <li><a href=""><img className="icon" src={mediumIcon}></img></a></li>
-                        <li><a href=""><img className="icon" src={emailIcon}></img></a></li>
+                        <li>
+                            <a href="https://www.facebook.com/Nimesha.Kahingala" target="_blank">
+                                <img className="icon" src={fbIcon}></img>
+
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/nimesha-kahingala/" target="_blank">
+                                <img className="icon" src={linkedinIcon}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img className="icon" src={mediumIcon}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:nimesha.isholi94@gmail.com">
+                                <img className="icon" src={emailIcon}></img>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>

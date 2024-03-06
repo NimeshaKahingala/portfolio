174bd811cbe37b01e796752d6cf568cc11ab09d8
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2024-03-06 14:11:32 -0700

    project images added

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,15 @@
 import img_millionSpaces from "../images/millionSpaces.png";
+import img_ms1 from "../images/ms1.PNG";
+import img_ms2 from "../images/ms2.PNG";
 import img_goldenGate from "../images/golden-gate.PNG";
+import img_goldenGate1 from "../images/golden-gate1.PNG";
+import img_goldenGate2 from "../images/golden-gate2.PNG";
 import img_1bt from "../images/1bt.PNG";
 import img_banyan from "../images/banyan.PNG";
+import img_banyan1 from "../images/banyan1.PNG";
+import img_sg from "../images/sg.PNG";
+import img_sg1 from "../images/sg1.PNG";
+import img_sg2 from "../images/sg2.PNG";
 
 export const PROJECTS = [{
     title: "MillionSpaces",
@@ -9,7 +17,8 @@ export const PROJECTS = [{
     description: "MillionSpaces is an online platform where customers can book their working space according to the facilities provided and also according to the environment they prefer",
     url: "https://millionspaces.com/Singapore",
     responsibilities : "Oversaw the transformation of design concepts into interactive web interfaces, enhancing the platform's user engagement and functionality. Developed responsive React components, ensuring a consistent user experience across various devices and platforms. Contributed to feature enhancements, refined user input validation processes, and error handling mechanisms. Successfully integrated complex backend APIs, including Google Map API, to augment the platform's capabilities.",
-    technology: "HTML, CSS, JavaScript, React, Ant Design, Google Map API, JWT"
+    technology: "HTML, CSS, JavaScript, React, Ant Design, Google Map API, JWT",
+    otherImages:[img_ms1,img_ms2]
 },
 {
     title: "Golden Gate",
@@ -17,15 +26,17 @@ export const PROJECTS = [{
     description: "Development of a student-centric application, `'Golden Gate,'` designed to enhance the educational experience through an interactive user interface.",
     url: "https://thegoldengate.uk/",
     responsibilities : "Developed a user-friendly interface, prioritizing accessibility and engagement for the student user base.",
-    technology: "HTML, CSS, JavaScript, React"
+    technology: "HTML, CSS, JavaScript, React",
+    otherImages:[img_goldenGate1,img_goldenGate2]
 },
 {
     title: "SqaudGurus",
-    image: "img-sg",
+    image: img_sg,
     description: "Squad Gurus is an online platform where clients can choose a skilled developer team to bring their business ideas to life within the agreed timeframe.",
     url: "https://www.squadgurus.com/",
     responsibilities : "Implementing cross-browser compatible, reusable, and maintainable code while adhering to web standards and best practices. Collaborate closely with other developers and designers to create pixel-perfect visual components to deliver intuitive and engaging user experiences. Played a crucial role in troubleshooting UI problems, and delivering solutions that enhanced overall user experience and interface stability. Scheduled and monitored knowledge transfer (KT) sessions based on project needs. Engaged in project estimations, contributing to accurate planning and resource allocation. Mentored and groomed newly joined team members, enhancing team skills and coherence.",
-    technology: "HTML, CSS, JavaScript, React, React-md, JWT"
+    technology: "HTML, CSS, JavaScript, React, React-md, JWT",
+    otherImages:[img_sg1, img_sg2]
 },
 {
     title: "1Billion Technology Website",
@@ -33,7 +44,8 @@ export const PROJECTS = [{
     description: "Overhaul and implement the official website of 1Billion Technology, adapting it to new and modern design principles.",
     url: "",
     responsibilities : "Led the front-end development process, transforming complex wireframes and sophisticated designs into dynamic web application interfaces, with a priority on mobile-first design principles to ensure optimal user experience across various devices. Ensured the creation of high-quality, cross-browser compatible, and maintainable code, emphasizing adherence to contemporary web standards and best practices.",
-    technology: " HTML, CSS, JavaScript, React, Tailwind CSS"
+    technology: " HTML, CSS, JavaScript, React, Tailwind CSS",
+    otherImages:[img_1bt]
 },
 {
     title: "Banyan Restaurant",
@@ -41,6 +53,7 @@ export const PROJECTS = [{
     description: "Development of a compelling online presence for Banyan Restaurant, focusing on responsive design and user engagement",
     url: "https://www.banyanrestaurant.co.uk/",
     responsibilities : "Translated wireframes and designs into web application interfaces. Implemented cross-browser compatible, reusable, and maintainable code while adhering to web standards and best practices.",
-    technology: "HTML, CSS, JavaScript, React"
+    technology: "HTML, CSS, JavaScript, React",
+    otherImages:[img_banyan,img_banyan1]
 }
 ];
--- src/components/portfolioModal.js
+++ src/components/portfolioModal.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { PROJECTS } from "../components/data";
 import "./portfolioModal.scss";
-import img_millionSpaces from "../images/millionSpaces.png";
-import img_goldenGate from "../images/goldenGate.PNG";
-
-// import * as iconSrc from '../images';
-
-
 
 
 function PortfolioModal(props) {
@@ -15,13 +9,17 @@ function PortfolioModal(props) {
         <>
 
             <div className='modal-wrap'>
-                <div className="img-wrap"><img className="image"></img>image</div>
+                <div className="img-wrap"><img className="image" src={props.data?.image} alt='website image'></img></div>
                 <h3 className="title">{props.data?.title}</h3>
                 <p className="description">{props.data?.description}</p>
                 <div className="technology">Technologies used : <span>{props.data?.technology}</span></div>
                 <div className="url">Website URL : <a href="https://millionspaces.com/Singapore" target="blank" className="link">millionspaces.com/Singapore</a></div>
                 <div className="website-images">
-                    <img></img>img1
+                    {props.data.otherImages.map(function (data) {
+                        return (
+                            <img src={data}></img>
+                        )
+                    })}
                 </div>
             </div>
 

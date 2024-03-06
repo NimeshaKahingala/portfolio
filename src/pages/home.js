7ecd3bb7787adaeb1f5ae895b7745a1b93048250
NimeshaKahingala <nimesha.kahingala@1billiontech.com>
2024-03-06 14:55:03 -0700

    theme color updated

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,10 +16,10 @@ function Home() {
                         <p className="job">Welcome! I’m Nimesha, a dynamic Front-End Developer with a flair for crafting compelling, user-first digital experiences. Armed with a Meta Front-End Developer Professional Certificate and over three years of hands-on experience, I thrive on utilizing HTML5, CSS3, JavaScript (ES6+), and cutting-edge frameworks like React and Redux to solve complex problems and create intuitive, accessible web applications. My journey has led me across continents, from the vibrant tech hubs of the United States and Singapore to my roots in Sri Lanka, allowing me to cultivate a unique blend of global insights and innovative approaches. Let’s embark on a journey to transform your digital ideas into reality!</p>
                         <div className="social">
                             <ul>
-                                <li><a href=""><img className="icon" src={fbIcon}></img></a></li>
-                                <li><a href=""><img className="icon" src={linkedinIcon}></img></a></li>
+                                <li><a href="https://www.facebook.com/Nimesha.Kahingala" target="_blank"><img className="icon" src={fbIcon}></img></a></li>
+                                <li><a href="https://www.linkedin.com/in/nimesha-kahingala/" target="_blank"><img className="icon" src={linkedinIcon}></img></a></li>
                                 <li><a href=""><img className="icon" src={mediumIcon}></img></a></li>
-                                <li><a href=""><img className="icon" src={emailIcon}></img></a></li>
+                                <li><a href="mailto:nimesha.isholi94@gmail.com"><img className="icon" src={emailIcon}></img></a></li>
                             </ul>
                         </div>
                     </div>
